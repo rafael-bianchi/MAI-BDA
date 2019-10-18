ec327430dd83df77bf308f934d3583cb0b20c5b2
Rafael Bianchi <rafaellfontana@gmail.com>
2019-10-18 20:00:22 -0300

    Added user's profile location

--- Exercise data gathering and storage from Twitter/analyze.py
+++ Exercise data gathering and storage from Twitter/analyze.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from collections import Counter
 import numpy as np
 import operator
+import geocoder as gc
+import requests
 
 # Establish connection with database
 client = MongoClient()
@@ -15,8 +17,9 @@ col = db.brexitSearch
 # Retrieve data from the mongodb database, choosing
 # the fields you'll need afterwards
 #######################################################
-my_tweets = db.brexitSearch.find({},{'lang':1, '_id':0, 'text':1, 'entities.hashtags':1,
-'in_reply_to_status_id':1, 'is_quote_status':1, 'retweeted_status':1, 'user.screen_name':1})
+my_tweets = db.brexitSearch.find({},{'lang':1, '_id':1, 'text':1, 'entities.hashtags':1,
+'in_reply_to_status_id':1, 'is_quote_status':1, 'retweeted_status':1, 'user.screen_name':1
+, 'user.location':1, 'geo':1, 'osm_location':1})
 numTweets = db.brexitSearch.count()
 
 ####################################################
@@ -74,7 +77,8 @@ hashList = []
 for t in my_tweets:
     for e in t['entities']['hashtags']:
         h = e['text']
-        hashList.append(h)
+        if h.lower() != 'brexit':
+			hashList.append(h.lower())
 D = Counter(hashList)
 subset = dict(D.most_common(15))
 sorted_subset = sorted(subset.iteritems(), key=operator.itemgetter(1))
@@ -88,4 +92,63 @@ plt.tight_layout()
 plt.show()
 
 
+##################################################################
+# Plotting most appeared countries
+##################################################################
 
+##################################################################
+# START - Getting information from geolocation on user's profile
+##################################################################
+my_tweets.rewind()
+locations = []
+tweets_without_location = []
+for t in my_tweets:
+	if 'osm_location' not in t.keys():
+		tweets_without_location.append(t)
+		if t['user']['location'] != None:
+			locations.append(t['user']['location'])
+
+geocoded = dict()
+loc_count = len(locations)
+with requests.Session() as session:
+	i = 1
+	for location in locations:
+		print('Checking location %s. (%s/%s)' %  (location, i, loc_count))
+		if location.lower() not in geocoded.keys():
+			g = gc.osm(location)
+			if (g != None and g.ok):
+				geocoded[location.lower()] = g
+
+		i = i + 1
+
+for t in tweets_without_location:
+	try:
+		if t['user']['location'] == None or t['user']['location'].lower() not in geocoded.keys() or geocoded[t['user']['location'].lower()].current_result.country_code == None:
+			db.brexitSearch.update({"_id" : t['_id'] }, {"$set" : {"osm_location": None}})
+		else:
+			db.brexitSearch.update({"_id" : t['_id'] }, {"$set" : {"osm_location": geocoded[t['user']['location'].lower()].current_result.country_code.upper()}})
+	except expression as identifier:
+		pass
+##################################################################
+# END - Getting information from geolocation on user's profile
+##################################################################
+
+
+my_tweets.rewind()
+countries = []
+for t in my_tweets:
+	country = t['osm_location']
+	if country != None:
+		countries.append(country.upper())
+
+D = Counter(countries)
+subset = dict(D.most_common(15))
+sorted_subset = sorted(subset.iteritems(), key=operator.itemgetter(1))
+
+# ----------- Horizontal Bar Plot ------------------------
+pos = range(len(sorted_subset))
+plt.barh(pos, [val[1] for val in sorted_subset], align = 'center', color = 'yellowgreen')
+plt.yticks(pos, [val[0] for val in sorted_subset])
+plt.title('Top 15 of countries captured')
+plt.tight_layout()
+plt.show()
